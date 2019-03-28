4444bc7cb6df26eb006aadf7a76ec16fc3021767
zeecnla <csrmelchor@gmail.com>
2019-03-28 16:55:40 -0700

    Added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,8 +8,19 @@ import twitter from '../images/assets/footer/twitter.svg'
 
 const Footer = ({ data }) => (
     <footer>
+        <h2>Let’s work together!</h2>
+
+        <p>
+            If you want to chat about a project you want help 
+            with or need any advice, just drop me a message. 
+            I’m AVAILABLE for any projects you want to work with.
+        </p>
+        <h3>Email Me</h3>
+        <p>Get My Resume</p>
+
+
+
         <div class="social">
-            <p>Let's Connect!</p>
             <ul>
                 <li>
                     <i src={github} />
