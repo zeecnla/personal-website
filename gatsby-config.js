e761ddca8288a49765ecc96acd05a8b2bd8262e7
zeecnla <csrmelchor@gmail.com>
2020-10-25 01:26:42 -0700

    removed config stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
-var spaceId;
-var accessToken;
+var spaceId
+var accessToken
 
-const environment = process.env.NODE_ENV;
+const environment = process.env.NODE_ENV
 
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`
-});
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 if (environment !== "production") {
-  spaceId = process.env.APP_SPACE_ID;
-  accessToken = process.env.APP_ACCESS_TOKEN;
-  trackingId = process.env.APP_TRACKING_ID;
+  spaceId = process.env.APP_SPACE_ID
+  accessToken = process.env.APP_ACCESS_TOKEN
+  trackingId = process.env.APP_TRACKING_ID
 } else {
-  spaceId = process.env.GATSBY_SPACE_ID;
-  accessToken = process.env.GATSBY_ACCESS_TOKEN;
-  trackingId = process.env.GATSBY_TRACKING_ID;
+  spaceId = process.env.GATSBY_SPACE_ID
+  accessToken = process.env.GATSBY_ACCESS_TOKEN
+  trackingId = process.env.GATSBY_TRACKING_ID
 }
 
 module.exports = {
@@ -26,17 +26,17 @@ module.exports = {
     social: [
       {
         name: "twitter",
-        url: "https://twitter.com/zeecnla"
+        url: "https://twitter.com/zeecnla",
       },
       {
         name: "twitch",
-        url: "https://www.twitch.tv/zeecnla"
+        url: "https://www.twitch.tv/zeecnla",
       },
       {
         name: "github",
-        url: "https://www.github.com/zeecnla"
-      }
-    ]
+        url: "https://www.github.com/zeecnla",
+      },
+    ],
   },
   plugins: [
     // Add support for *.mdx files in gatsby
@@ -45,48 +45,48 @@ module.exports = {
 
     // Add a collection called "posts" that looks
     // for files in content/posts/
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "posts",
-        path: `${__dirname}/content/posts/`
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: `${__dirname}/content/images`
-      }
-    },
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "posts",
+    //     path: `${__dirname}/content/posts/`
+    //   }
+    // },
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "images",
+    //     path: `${__dirname}/content/images`
+    //   }
+    // },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-react-svg`,
       options: {
         rule: {
-          include: /\.inline\.svg$/
-        }
-      }
+          include: /\.inline\.svg$/,
+        },
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: trackingId
-      }
+        trackingId: trackingId,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: "footer",
-        path: `${__dirname}/src/images/assets/footer`
-      }
+        path: `${__dirname}/src/images/assets/footer`,
+      },
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: spaceId,
-        accessToken: accessToken
-      }
+        accessToken: accessToken,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
@@ -100,11 +100,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png` // This path is relative to the root of the site.
-      }
-    }
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ]
-};
+  ],
+}
