a1d794984b84f961143636ce27726ac700cadd30
Cesar <csrmelchor@gmail.com>
2021-10-28 00:54:07 -0700

    fixed aos styles

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,18 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import Navbar from "../Navbar/navbar"
 import Footer from "../Footer/footer"
+import "aos/dist/aos.css"
 import "./layout.css"
 import "font-awesome/css/font-awesome.min.css"
 import { device } from "../device"
 import "animate.css"
-
-if (typeof document !== `undefined`) {
-  var AOS = require("aos")
-  AOS.init()
-}
 
 const Container = styled.div`
   padding: 0 20px 0px 20px;
@@ -29,28 +25,48 @@ const Container = styled.div`
   }
 `
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+const Layout = ({ children }) => {
+  let AOS
+  useEffect(() => {
+    /**
+     * Server-side rendering does not provide the 'document' object
+     * therefore this import is required either in useEffect or componentDidMount as they
+     * are exclusively executed on a client
+     */
+    const AOS = require("aos")
+    AOS.init({
+      once: true,
+    })
+  }, [])
+
+  useEffect(() => {
+    if (AOS) {
+      AOS.refresh()
+    }
+  })
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={(data) => (
-      <div style={{ minHeight: "100vh", position: `relative` }}>
-        <Navbar />
-        <Container>
-          <main>{children}</main>
-        </Container>
-        <Footer />
-      </div>
-    )}
-  />
-)
+      `}
+      render={(data) => (
+        <div style={{ minHeight: "100vh", position: `relative` }}>
+          <Navbar />
+          <Container>
+            <main>{children}</main>
+          </Container>
+          <Footer />
+        </div>
+      )}
+    />
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
